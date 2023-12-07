d602c5ee267e32a79adb9ee376461ab5a6d6a75d
Anak-caetano <anak.94c@gmail.com>
2023-12-07 15:10:28 -0300

    integração com fullcalendar

--- src/config/listar-evento.php
+++ src/config/listar-evento.php
@@ -1,10 +1,19 @@
 <?php
 
-include_once 'conexao.php';
+$hostname = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'calendariodb';
+
+$conexao = new mysqli($hostname, $username, $password, $database);
+    if ($conexao->connect_errno) {
+        echo "Falha ao conectar: (" .$conexao->connect_errno . ") " . $conexao->connect_error;
+
+}
 
 $query_events = "SELECT id, title, color, start, end FROM events";
 
-$result_events = $conn->prepare($query_events);
+$result_events = $conexao->prepare($query_events);
 
 $result_events->execute();
 
